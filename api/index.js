5c5ab4814f1f6408794a940d0c9b4f5883a5d144
chiragthakkar0012 <chiragthakkar0012@gmail.com>
2024-12-30 16:07:31 +0530

    connected to databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then((d)=>console.log('coonected to mongodb',d)).catch((err)=>console.log(err))
 const app=express();
 
 app.listen(3000,()=>
